c9651da41511e69feed0cf04c4570fd95dbb92ea
MosinyanR <mosinyan.js@gmail.com>
2023-02-18 14:35:49 +0400

    change index js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/Sergey/App';
+import App from './components/Raf/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
