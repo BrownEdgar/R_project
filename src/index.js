4522bc2b8b2ec00995b4149860ab182f7450608f
MosinyanR <mosinyan.js@gmail.com>
2023-02-20 15:53:41 +0400

    HomeWork20.02.23

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./components/Sergey/Homework_2/App"; //tnnayini hasce
-import App from "./components/Sergey/Homework_2/App";
+import App from "./components/Raf/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
