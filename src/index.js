3db3c4939caf66ea420d231a86a96fd37cd73e97
Sergey Petrosyan <petrosyanserg33@gmail.com>
2023-02-24 22:24:07 +0400

    Add Homework_4 (25/02/2023)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./components/Edgar/workwithImages/App";
-import App from "./components/Edgar/fetch/App";
+//import App from "./components/Sergey/Homework_4/GitHubSearch/App";
+//import App from "./components/Sergey/Homework_4/AutoCards/App";
+import App from "./components/Edgar/workwithImages/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-		<App />
-);
+root.render(<App />);
