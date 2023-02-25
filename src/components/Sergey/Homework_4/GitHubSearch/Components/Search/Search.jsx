362e1d812c9fbee42687a822052aabc2fdcba7c2
BrownEdgar <nikogosjanedgar@gmail.com>
2023-02-25 13:09:45 +0400

    fix code style

--- src/components/Sergey/Homework_4/GitHubSearch/Components/Search/Search.jsx
+++ src/components/Sergey/Homework_4/GitHubSearch/Components/Search/Search.jsx
@@ -2,30 +2,30 @@ import React, { useState } from "react";
 import style from "./Search.module.scss";
 import { BsSearch } from "react-icons/bs";
 
-export default function Search({ Search }) {
-  const [inputText, setInputText] = useState("");
+export default function Search({ search }) {
+	const [inputText, setInputText] = useState("");
 
-  function Submit(event) {
-    event.preventDefault();
-    Search(inputText);
-  }
-  return (
-    <div className={style.Search}>
-      <form className={style.Search_Form} onSubmit={Submit}>
-        <label htmlFor="search">
-          <BsSearch className={style.Search_Icon} />
-        </label>
-        <input
-          className={style.Search_Input}
-          type="search"
-          placeholder="Search GitHub Username..."
-          id="search"
-          value={inputText}
-          onChange={(event) => setInputText(event.target.value)}
-          required
-        />
-        <button className={style.Search_Button}>Search</button>
-      </form>
-    </div>
-  );
+	const submit = (event) => {
+		event.preventDefault();
+		search(inputText);
+	}
+	return (
+		<div className={style.Search}>
+			<form className={style.Search_Form} onSubmit={submit}>
+				<label htmlFor="search">
+					<BsSearch className={style.Search_Icon} />
+				</label>
+				<input
+					className={style.Search_Input}
+					type="search"
+					placeholder="Search GitHub Username..."
+					id="search"
+					value={inputText}
+					onChange={(event) => setInputText(event.target.value)}
+					required
+				/>
+				<button className={style.Search_Button}>Search</button>
+			</form>
+		</div>
+	);
 }
