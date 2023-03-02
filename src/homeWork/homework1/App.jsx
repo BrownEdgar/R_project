df53bf933c05d347e7b02caaf5ab6db413b7573d
BrownEdgar <nikogosjanedgar@gmail.com>
2023-03-02 13:27:59 +0400

    fixed wrong hemework version

--- src/homeWork/homework1/App.jsx
+++ src/homeWork/homework1/App.jsx
@@ -1,26 +1,29 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+
 import UserData from './components/UserData';
+import useData from './components/useData'
+
+import "./App.scss"
 
 export default function App() {
-    const [users,setUsers] = useState([{}])
-    const [count, setCount] = useState(0);
-    const countChange = () =>{
-        count < 11 ? setCount(count + 1) : setCount(0)
-    } 
-    useEffect(() => {
-        const getPostsData = () => {
-            axios
-            .get("https://jsonplaceholder.typicode.com/posts?_limit=13")
-            .then(data => setUsers(data.data))
-            .catch(error => console.log(error));
-            };
-           getPostsData();
-    },[])
+    // const [users,setUsers] = useState([{}])
+ 
+    // useEffect(() => {
+    //     const getPostsData = () => {
+    //         axios
+    //         .get("https://jsonplaceholder.typicode.com/posts?_limit=13")
+    //         .then(data => setUsers(data.data))
+    //         .catch(error => console.log(error));
+    //         };
+    //        getPostsData();
+    // },[])
+
+	const [user , countChange ]  = useData()
+	console.log(user)
 
   return (
-    <div>
-        <UserData users={users} count={count}/>
+    <div className='App'>
+			{Object.keys(user).length > 0 && <UserData user={user} />}
         <button onClick={countChange}>Change</button>
     </div>
   )
--- src/homeWork/homework1/components/UserData.jsx
+++ src/homeWork/homework1/components/UserData.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
-export default function ({users,count}) {
-    
+export default function UserData({user}) {
   return (
     <div>
-        <h2>get post N{users[count].id} Every <span>Click</span> time fetch</h2>
-        <p>{users[count].id}</p>
-        <p>{users[count].title}</p>
-        <p>{users[count].body}</p>
+        <h2>get post N{user?.id} Every <span>Click</span> time fetch</h2>
+        <p>{user?.id}</p>
+        <p>{user?.title}</p>
+        <p>{user?.body}</p>
     </div>
   )
 }
